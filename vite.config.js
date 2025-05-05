7ecac7175a39f6c5d2ca8516b694e6f67bba145a
lnfugh <85252190+lnfugh@users.noreply.github.com>
2025-05-05 15:36:01 +0000

    Initial Build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pp-xml-builder/",
+  base: "/xml-builder/",
   assetsInclude: ['**/*.xml', '**/*.xsd'],
   css: {
     modules: true
